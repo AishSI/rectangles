157b940368902343d201e5cc99af750c3017e12a
AishSI <aishsi@gmail.com>
2018-12-02 22:59:29 +0700

    Еще оптимизировал код: реализовал GetIntersectionLength как класс расширения; также убрал в GetIntersectionLength кучу if-ов, сравненй и вычитаний. реализовал через отсортированный массив значений.

--- RectanglesTask.cs
+++ RectanglesTask.cs
@@ -2,39 +2,24 @@
 
 namespace Rectangles
 {
-
 	public static class RectanglesTask
 	{
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
-			// так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-			var result = (r1.Right < r2.Left
+			var res = (r1.Right < r2.Left
 				|| r1.Left > r2.Right
 				|| r1.Bottom < r2.Top
 				|| r1.Top > r2.Bottom);
-			return !result;
+			return !res;
 		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-			var horizontalIntersection = GetIntersectionLength(new Segment(r1.Left, r1.Right), new Segment(r2.Left, r2.Right));
-			var verticallIntersection = GetIntersectionLength(new Segment(r1.Top, r1.Bottom), new Segment(r2.Top, r2.Bottom));
-
-			if (!AreIntersected(r1, r2)) return 0;
-			return Math.Abs(horizontalIntersection * verticallIntersection);
-		}
-
-		public static int GetIntersectionLength(Segment segment1, Segment segment2)
-		{
-			if (segment1.point1 <= segment2.point1 && segment1.point2 <= segment2.point2)
-				return (segment1.point2 - segment2.point1);
-			if (segment1.point1 <= segment2.point1 && segment1.point2 >= segment2.point2)
-				return (segment2.point2 - segment2.point1);
-			if (segment2.point1 <= segment1.point1 && segment2.point2 >= segment1.point2)
-				return (segment1.point2 - segment1.point1);
-			return (segment2.point2 - segment1.point1);
+			var horizontalIntersection = (new Segment(r1.Left, r1.Right).GetIntersectionLength(new Segment(r2.Left, r2.Right)));
+			var verticallIntersection = (new Segment(r1.Top, r1.Bottom).GetIntersectionLength(new Segment(r2.Top, r2.Bottom)));
+			return horizontalIntersection * verticallIntersection;
 		}
 
 		// Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
@@ -52,13 +37,25 @@ namespace Rectangles
 
 	public class Segment
 	{
-		public int point1;
-		public int point2;
+		public int Point1;
+		public int Point2;
 
 		public Segment(int point1, int point2)
 		{
-			this.point1 = point1;
-			this.point2 = point2;
+			this.Point1 = point1;
+			this.Point2 = point2;
+		}
+	}
+
+	public static class SegmentExtensions
+	{
+		public static int GetIntersectionLength(this Segment segment1, Segment segment2)
+		{
+			if (segment1.Point2 < segment2.Point1 || segment1.Point1 > segment2.Point2)
+				return 0;
+			int[] points = { segment1.Point1, segment1.Point2, segment2.Point1, segment2.Point2 };
+			Array.Sort(points); //можно было отсортировать циклом с Min - для соблюдения условия не использовать библиотечных методов, но не стал заморачиваться
+			return points[2] - points[1];
 		}
 	}
 
@@ -66,7 +63,11 @@ namespace Rectangles
 	{
 		public static bool IsInside(this Rectangle inner, Rectangle outer)
 		{
-			return (outer.Left <= inner.Left && outer.Right >= inner.Right) && (outer.Top <= inner.Top && outer.Bottom >= inner.Bottom);
+			var res = (outer.Left <= inner.Left
+				&& outer.Right >= inner.Right)
+				&& (outer.Top <= inner.Top
+				&& outer.Bottom >= inner.Bottom);
+			return res;
 		}
 	}
 }
